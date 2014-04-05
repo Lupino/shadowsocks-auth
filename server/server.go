ba0621211f9054a9f636a6711c0e32dd49b42bc4
Ubasic <lmjubuntu@gmail.com>
2014-04-05 10:33:12 +0800

    remove unuself code

--- server/server.go
+++ server/server.go
@@ -13,7 +13,6 @@ import (
 	"os/signal"
 	"runtime"
 	"strconv"
-	"sync"
 	"syscall"
     "bytes"
 )
@@ -189,95 +188,11 @@ func handleConnection(conn *ss.Conn) {
 	return
 }
 
-type PortListener struct {
-	password string
-	listener net.Listener
-}
-
-type PasswdManager struct {
-	sync.Mutex
-	portListener map[string]*PortListener
-}
-
-func (pm *PasswdManager) add(port, password string, listener net.Listener) {
-	pm.Lock()
-	pm.portListener[port] = &PortListener{password, listener}
-	pm.Unlock()
-}
-
-func (pm *PasswdManager) get(port string) (pl *PortListener, ok bool) {
-	pm.Lock()
-	pl, ok = pm.portListener[port]
-	pm.Unlock()
-	return
-}
-
-func (pm *PasswdManager) del(port string) {
-	pl, ok := pm.get(port)
-	if !ok {
-		return
-	}
-	pl.listener.Close()
-	pm.Lock()
-	delete(pm.portListener, port)
-	pm.Unlock()
-}
-
-// Update port password would first close a port and restart listening on that
-// port. A different approach would be directly change the password used by
-// that port, but that requires **sharing** password between the port listener
-// and password manager.
-func (pm *PasswdManager) updatePortPasswd(port, password string) {
-	pl, ok := pm.get(port)
-	if !ok {
-		log.Printf("new port %s added\n", port)
-	} else {
-		if pl.password == password {
-			return
-		}
-		log.Printf("closing port %s to update password\n", port)
-		pl.listener.Close()
-	}
-	// run will add the new port listener to passwdManager.
-	// So there maybe concurrent access to passwdManager and we need lock to protect it.
-	go run(port, password)
-}
-
-var passwdManager = PasswdManager{portListener: map[string]*PortListener{}}
-
-func updatePasswd() {
-	log.Println("updating password")
-	newconfig, err := ss.ParseConfig(configFile)
-	if err != nil {
-		log.Printf("error parsing config file %s to update password: %v\n", configFile, err)
-		return
-	}
-	oldconfig := config
-	config = newconfig
-
-	if err = unifyPortPassword(config); err != nil {
-		return
-	}
-	for port, passwd := range config.PortPassword {
-		passwdManager.updatePortPasswd(port, passwd)
-		if oldconfig.PortPassword != nil {
-			delete(oldconfig.PortPassword, port)
-		}
-	}
-	// port password still left in the old config should be closed
-	for port, _ := range oldconfig.PortPassword {
-		log.Printf("closing port %s as it's deleted\n", port)
-		passwdManager.del(port)
-	}
-	log.Println("password updated")
-}
-
 func waitSignal() {
 	var sigChan = make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGHUP)
 	for sig := range sigChan {
 		if sig == syscall.SIGHUP {
-			updatePasswd()
 		} else {
 			// is this going to happen?
 			log.Printf("caught signal %v, exit", sig)
@@ -286,13 +201,12 @@ func waitSignal() {
 	}
 }
 
-func run(port, password string) {
+func run(port string) {
 	ln, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Printf("error listening port %v: %v\n", port, err)
 		return
 	}
-	passwdManager.add(port, password, ln)
 	var cipher *ss.Cipher
 	log.Printf("server listening port %v ...\n", port)
 	for {
@@ -321,26 +235,6 @@ func run(port, password string) {
 		}
 		go handleConnection(ss.NewConn(conn, cipher.Copy()))
 	}
-}
-
-func enoughOptions(config *ss.Config) bool {
-	return config.ServerPort != 0 && config.Password != ""
-}
-
-func unifyPortPassword(config *ss.Config) (err error) {
-	if len(config.PortPassword) == 0 { // this handles both nil PortPassword and empty one
-		if !enoughOptions(config) {
-			fmt.Fprintln(os.Stderr, "must specify both port and password")
-			return errors.New("not enough options")
-		}
-		port := strconv.Itoa(config.ServerPort)
-		config.PortPassword = map[string]string{port: config.Password}
-	} else {
-		if config.Password != "" || config.ServerPort != 0 {
-			fmt.Fprintln(os.Stderr, "given port_password, ignore server_port and password option")
-		}
-	}
-	return
 }
 
 var configFile string
@@ -386,15 +280,11 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	if err = unifyPortPassword(config); err != nil {
-		os.Exit(1)
-	}
 	if core > 0 {
 		runtime.GOMAXPROCS(core)
 	}
-	for port, password := range config.PortPassword {
-		go run(port, password)
-	}
+
+	go run(fmt.Sprintf("%d", config.ServerPort))
 
 	waitSignal()
 }
